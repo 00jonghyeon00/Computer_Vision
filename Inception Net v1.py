cabf21994b2cecd9fc198d2fa7747dbd82e1fe52
00jonghyeon00 <113824396+00jonghyeon00@users.noreply.github.com>
2024-05-23 21:35:29 +0900

    Update Inception Net v1.py

--- Inception Net v1.py
+++ Inception Net v1.py
@@ -69,5 +69,106 @@ class InceptionAux(nn.Module):
         # N x 1000(num_classes)
         return x
 
+class Inception_V1(nn.Module):
+    def __init__(self, num_classes = 1000, use_aux=True, init_weights=None, drop_p=0.4, drop_p_aux=0.7):
+        super().__init__()
+
+        self.use_aux = use_aux
+
+        self.conv1 = BasicConv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.maxpool1 = nn.MaxPool2d(3, stride=2, padding=1)
+        self.conv2a = BasicConv2d(64, 64, kernel_size=1)
+        self.conv2b = BasicConv2d(64, 192, kernel_size=3, padding=1)
+        self.maxpool2 = nn.MaxPool2d(3, stride=2, padding=1)
+        self.inception3a = Inception(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = Inception(256, 128, 128, 192, 32, 96, 64)
+        self.maxpool3 = nn.MaxPool2d(3, stride=2, padding=1)
+        self.inception4a = Inception(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = Inception(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = Inception(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = Inception(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = Inception(528, 256, 160, 320, 32, 128, 128)
+        self.maxpool4 = nn.MaxPool2d(3, stride=2, padding=1)
+        self.inception5a = Inception(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = Inception(832, 384, 192, 384, 48, 128, 128)
+        
+        if use_aux:
+            self.aux1 = InceptionAux(512, num_classes, drop_p=drop_p_aux)
+            self.aux2 = InceptionAux(528, num_classes, drop_p=drop_p_aux)
+        else:
+            self.aux1 = None
+            self.aux2 = None
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1)) # GAP
+        self.dropout = nn.Dropout(p=drop_p)
+        self.fc = nn.Linear(1024, num_classes)
+
+        if init_weights:
+            for m in self.modules():
+                if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                    torch.nn.init.trunc_normal_(m.weight, mean=0.0, std=0.01, a=-2, b=2)
+    
+    def forward(self, x):
+        # N x 3 x 224 x 224
+        x = self.conv1(x)
+        # N x 64 x 112 x 112
+        x = self.maxpool1(x)
+        # N x 64 x 56 x 56
+        x = self.conv2a(x)
+        # N x 64 x 56 x 56
+        x = self.conv2b(x)
+        # N x 192 x 56 x 56
+        x = self.maxpool2(x)
+
+        # N x 192 x 28 x 28
+        x = self.inception3a(x)
+        # N x 256 x 28 x 28
+        x = self.inception3b(x)
+        # N x 480 x 28 x 28
+        x = self.maxpool3(x)
+        # N x 480 x 14 x 14
+        x = self.inception4a(x)
+
+        # N x 512 x 14 x 14
+        if self.aux1 is not None and self.training:
+            aux1 = self.aux1(x)
+        else:
+            aux1 = None  # 뭐라도 넣어놔야해서 not defined error
+
+        x = self.inception4b(x)
+        # N x 512 x 14 x 14
+        x = self.inception4c(x)
+        # N x 512 x 14 x 14
+        x = self.inception4d(x)
+
+        # N x 528 x 14 x 14
+        if self.aux2 is not None and self.training:
+            aux2 = self.aux2(x)
+        else:
+            aux2 = None
+
+        x = self.inception4e(x)
+        # N x 832 x 14 x 14
+        x = self.maxpool4(x)
+        # N x 832 x 7 x 7
+        x = self.inception5a(x)
+        # N x 832 x 7 x 7
+        x = self.inception5b(x)
+        # N x 1024 x 7 x 7
+
+        x = self.avgpool(x)
+        # N x 1024 x 1 x 1
+        x = torch.flatten(x, 1)
+        # N x 1024
+        x = self.dropout(x)
+        x = self.fc(x)
+        # N x 1000 (num_classes)
+        return x, aux2, aux1
+
+# x, aux2, aux1 = model(torch.randn(2,3,224,224))
+# print(x.shape)
+# print(aux2.shape)
+# print(aux1.shape)
+
 
 
